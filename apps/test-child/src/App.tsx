2e661add4d29e882445aecd52cbe8f96bcaaed55
Dreadhalor <Scotthetrick2@yahoo.com>
2024-02-01 19:54:34 -0800

    Well, it works! Portfolio can now host test-child in an iframe & they can communicate back & forth

--- apps/test-child/src/App.tsx
+++ apps/test-child/src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "dread-ui";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+  const [parentMessage, setParentMessage] = useState("");
+
+  useEffect(() => {
+    const handler = (ev: MessageEvent<{ type: string; message: string }>) => {
+      if (typeof ev.data !== "object") return;
+      if (!ev.data.type) return;
+      if (ev.data.type !== "button-click") return;
+      if (!ev.data.message) return;
+
+      setParentMessage(ev.data.message);
+    };
+
+    window.addEventListener("message", handler);
+
+    // Don't forget to remove addEventListener
+    return () => window.removeEventListener("message", handler);
+  }, []);
+
+  const clicked = (msg: string) => {
+    window.parent.postMessage(
+      {
+        type: "button-click",
+        message: msg,
+      },
+      "*",
+    );
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex h-full w-full flex-col items-center justify-center gap-4 border-4 border-white">
+      Hi, I am a child app.
+      <div className="flex gap-2">
+        <input
+          placeholder="Send message to parent"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+        <Button label="Talk to parent" onClick={() => clicked(message)} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      Parent says: {parentMessage}
+    </div>
+  );
 }
 
-export default App
+export { App };
