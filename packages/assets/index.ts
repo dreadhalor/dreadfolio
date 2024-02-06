bd87b01c8b8f61763d423c0b98d69f36d2f00d42
Dreadhalor <Scotthetrick2@yahoo.com>
2024-02-05 23:22:42 -0800

    Continuing to clean up files Added Accordion to dread-ui

--- packages/assets/index.ts
+++ packages/assets/index.ts
@@ -1,3 +1,2 @@
 export * from "./icons";
 export * from "./app-images";
-// export * as AppSpaceMask from "./app-space-mask.svg";
---
+++ packages/dread-ui/src/lib/icons.ts
@@ -0,0 +1,12 @@
+export {
+  FaCaretRight as Caret,
+  FaCopy as Copy,
+  FaChevronRight as ChevronRight,
+  FaChevronDown as ChevronDown,
+  FaEdit as Edit,
+  FaRegTrashAlt as TrashCan,
+} from "react-icons/fa";
+export {
+  BsGlobe as Globe,
+  BsThreeDotsVertical as DotsMenuVertical,
+} from "react-icons/bs";
