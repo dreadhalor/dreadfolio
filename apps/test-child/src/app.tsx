74f868b90f10f80b02a2548fef6b712434888ba9
Dreadhalor <Scotthetrick2@yahoo.com>
2024-02-10 02:13:33 -0800

    Updated vite config to use vite-tsconfig-paths

--- apps/test-child/src/app.tsx
+++ apps/test-child/src/app.tsx
@@ -1,46 +1,46 @@
-import { Button } from "dread-ui";
-import { useEffect, useState } from "react";
+import { Button } from 'dread-ui';
+import { useEffect, useState } from 'react';
 
 function ChildApp() {
-  const [message, setMessage] = useState("");
-  const [parentMessage, setParentMessage] = useState("");
+  const [message, setMessage] = useState('');
+  const [parentMessage, setParentMessage] = useState('');
 
   useEffect(() => {
     const handler = (ev: MessageEvent<{ type: string; message: string }>) => {
-      if (typeof ev.data !== "object") return;
+      if (typeof ev.data !== 'object') return;
       if (!ev.data.type) return;
-      if (ev.data.type !== "button-click") return;
+      if (ev.data.type !== 'button-click') return;
       if (!ev.data.message) return;
 
       setParentMessage(ev.data.message);
     };
 
-    window.addEventListener("message", handler);
+    window.addEventListener('message', handler);
 
     // Don't forget to remove addEventListener
-    return () => window.removeEventListener("message", handler);
+    return () => window.removeEventListener('message', handler);
   }, []);
 
   const clicked = (msg: string) => {
     window.parent.postMessage(
       {
-        type: "button-click",
+        type: 'button-click',
         message: msg,
       },
-      "*",
+      '*',
     );
   };
 
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center gap-4 border-4 border-white">
+    <div className='flex h-full w-full flex-col items-center justify-center gap-4 border-4 border-white'>
       Hi, I am a child app.
-      <div className="flex gap-2">
+      <div className='flex gap-2'>
         <input
-          placeholder="Send message to parent"
+          placeholder='Send message to parent'
           onChange={(e) => setMessage(e.target.value)}
           value={message}
         />
-        <Button label="Talk to parent" onClick={() => clicked(message)} />
+        <Button onClick={() => clicked(message)}>Talk to parent</Button>
       </div>
       Parent says: {parentMessage}
     </div>
