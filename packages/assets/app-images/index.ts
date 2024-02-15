980f16ab36e17ebe49e72dd4d6f064efc6313857
Dreadhalor <Scotthetrick2@yahoo.com>
2024-02-15 01:25:02 -0800

    Updates to portfolio frontend Added shareme assets

--- packages/assets/app-images/index.ts
+++ packages/assets/app-images/index.ts
@@ -1,4 +1,5 @@
-export { default as EnlightScreenshot } from "./enlight.png";
-export { default as MinesweeperScreenshot } from "./minesweeper.png";
-export { default as PathfinderVisualizerScreenshot } from "./pathfinder-visualizer.png";
-export { default as AsciiVideoScreenshot } from "./ascii-video.jpg";
+export { default as EnlightScreenshot } from './enlight.png';
+export { default as MinesweeperScreenshot } from './minesweeper.png';
+export { default as PathfinderVisualizerScreenshot } from './pathfinder-visualizer.png';
+export { default as AsciiVideoScreenshot } from './ascii-video.jpg';
+export { default as ShareMeScreenshot } from './shareme.png';
--- packages/assets/icons/index.ts
+++ packages/assets/icons/index.ts
@@ -1,4 +1,5 @@
-export { default as EnlightIcon } from "./enlight-icon.svg";
-export { default as MinesweeperIcon } from "./minesweeper-icon.svg";
-export { default as PathfinderVisualizerIcon } from "./pathfinder-visualizer-icon.svg";
-export { default as AsciiVideoIcon } from "./ascii-video-icon.svg";
+export { default as EnlightIcon } from './enlight-icon.svg';
+export { default as MinesweeperIcon } from './minesweeper-icon.svg';
+export { default as PathfinderVisualizerIcon } from './pathfinder-visualizer-icon.svg';
+export { default as AsciiVideoIcon } from './ascii-video-icon.svg';
+export { default as ShareMeIcon } from './shareme-icon.svg';
