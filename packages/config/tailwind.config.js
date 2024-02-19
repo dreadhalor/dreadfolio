4cd7aea19a64a6d994ee1e71e04ea249402836ab
Dreadhalor <Scotthetrick2@yahoo.com>
2024-02-18 21:37:19 -0800

    gifster updates dread-ui updates

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -68,6 +68,8 @@ export default {
       },
       fontFamily: {
         sans: ['var(--font-sans)', ...fontFamily.sans],
+        serif: ['var(--font-serif)', ...fontFamily.serif],
+        mono: ['var(--font-mono)', ...fontFamily.mono],
       },
       keyframes: {
         'accordion-down': {
