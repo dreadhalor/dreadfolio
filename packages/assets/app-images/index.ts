b5be94cade33b4634f0b983af330fe67d8e102c4
Dreadhalor <Scotthetrick2@yahoo.com>
2024-02-19 04:05:24 -0800

    Added assets

--- packages/assets/app-images/index.ts
+++ packages/assets/app-images/index.ts
@@ -4,3 +4,5 @@ export { default as PathfinderVisualizerScreenshot } from './pathfinder-visualiz
 export { default as AsciiVideoScreenshot } from './ascii-video.jpg';
 export { default as ShareMeScreenshot } from './shareme.png';
 export { default as SuDoneKuScreenshot } from './su-done-ku.png';
+export { default as GifsterScreenshot } from './gifster.png';
+export { default as DreadUiScreenshot } from './dread-ui.png';
