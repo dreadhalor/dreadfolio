6ee1420349904c410d0e6fa80b79ce714d7cb6de
Dreadhalor <Scotthetrick2@yahoo.com>
2024-02-26 02:25:56 -0800

    circle-mesh works, but you have to move the mouse very slowly

--- apps/sketches/src/sketches/circle-mesh/circle-mesh.ts
+++ apps/sketches/src/sketches/circle-mesh/circle-mesh.ts
@@ -1,32 +1,49 @@
-import { Link } from './link';
 import P5 from 'p5';
+import { Link } from './link';
 
-export const unitRadius = 20;
-export const linkGap = 50;
+export const attractionDist = 50;
+export const repelDist = Math.sqrt(2) * attractionDist;
 
 export const CircleMesh = (p5: P5) => {
   const links: Link[] = [];
-  const layers = 13;
-
   p5.setup = () => {
     p5.createCanvas(p5.windowWidth, p5.windowHeight);
-    for (let i = 0; i < layers; i++) {
-      for (let j = 0; j < layers; j++) {
-        links.push(
-          new Link(
-            p5,
-            p5.createVector(
-              i * linkGap - (linkGap * (layers - 1)) / 2,
-              j * linkGap - (linkGap * (layers - 1)) / 2,
-            ),
-          ),
-        );
+    const center = p5.createVector(p5.width / 2, p5.height / 2);
+    // create a 9x9 grid of links
+    // connect them to their neighbors and diagonal neighbors
+    // make the center one follow the mouse
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        const x = p5.map(i, 0, 8, center.x - 200, center.x + 200);
+        const y = p5.map(j, 0, 8, center.y - 200, center.y + 200);
+        const link = new Link(p5, p5.createVector(x, y));
+        links.push(link);
       }
     }
+
+    links.forEach((link, i) => {
+      if (i % 9 !== 0) {
+        link.connect(links[i - 1]);
+      }
+      if (i > 8) {
+        link.connect(links[i - 9]);
+      }
+      if (i % 9 !== 0 && i > 8) {
+        link.connectDiagonal(links[i - 10]);
+      }
+      if (i % 9 !== 8 && i > 8) {
+        link.connectDiagonal(links[i - 8]);
+      }
+    });
+
+    links[40].followsMouse = true;
   };
 
   p5.draw = () => {
     p5.background(255);
+    links.forEach((link) => {
+      link.calculateTick();
+    });
     links.forEach((link) => {
       link.tick();
       link.draw();
--- apps/sketches/src/sketches/circle-mesh/link.ts
+++ apps/sketches/src/sketches/circle-mesh/link.ts
@@ -1,25 +1,103 @@
 import P5 from 'p5';
-import { unitRadius } from './circle-mesh';
+import { attractionDist } from './circle-mesh';
 export class Link {
   p5: P5;
   pos: P5.Vector;
+  vel: P5.Vector;
+  acc: P5.Vector;
+  adjacentNeighbors: Link[] = [];
+  diagonalNeighbors: Link[] = [];
+  k: number = 5;
+  followsMouse = false;
 
-  constructor(p5: P5, pos: P5.Vector) {
+  constructor(p5: P5, pos: P5.Vector, followsMouse = false) {
     this.p5 = p5;
     this.pos = pos;
+    this.vel = p5.createVector(0, 0);
+    this.acc = p5.createVector(0, 0);
+    this.followsMouse = followsMouse;
   }
 
-  tick() {}
+  calculateTick() {
+    // Apply a force towards the mouse position
+    if (this.followsMouse) {
+      const mouseForce = this.getMouseForce();
+      this.vel.add(mouseForce);
+      this.vel.mult(0.2); // Damping mouse spring differently for now
+    } else {
+      const neighborForce = this.getNeighborForce();
+      this.vel.add(neighborForce);
+      this.vel.mult(0.9); // Damping
+    }
+  }
+  tick() {
+    this.pos.add(this.vel);
+    // this.acc.mult(0);
+  }
 
   draw() {
-    this.p5.push();
-    this.p5.fill(255); // Fill color if needed
-    this.p5.stroke(0);
-    this.p5.circle(
-      this.p5.mouseX + this.pos.x,
-      this.p5.mouseY + this.pos.y,
-      unitRadius * 2,
-    );
-    this.p5.pop();
+    this.adjacentNeighbors.forEach((neighbor) => {
+      const springForce = this.calculateSpringForce(neighbor);
+      this.p5.stroke(0);
+      this.p5.line(
+        this.pos.x,
+        this.pos.y,
+        this.pos.x + springForce.x,
+        this.pos.y + springForce.y,
+      );
+    });
+    this.p5.ellipse(this.pos.x, this.pos.y, 20);
   }
+
+  getNeighborForce = () => {
+    const neighborForce = this.p5.createVector(0, 0);
+    // they should be exactly dist apart from each other
+    this.adjacentNeighbors.forEach((neighbor) => {
+      const force = P5.Vector.sub(neighbor.pos, this.pos);
+      const distance = force.mag();
+      const forceMag = (distance - attractionDist) / 10;
+      force.setMag(forceMag * 3);
+      neighborForce.add(force);
+    });
+    this.diagonalNeighbors.forEach((neighbor) => {
+      const force = P5.Vector.sub(neighbor.pos, this.pos);
+      const distance = force.mag();
+      const forceMag = (distance - attractionDist * Math.sqrt(2)) / 10;
+      force.setMag(forceMag * 3);
+      neighborForce.add(force);
+    });
+    return neighborForce;
+  };
+
+  getMouseForce = () => {
+    // exit if the mouse is at 0,0
+    if (this.p5.mouseX === 0 && this.p5.mouseY === 0)
+      return this.p5.createVector(0, 0);
+    const mousePos = this.p5.createVector(this.p5.mouseX, this.p5.mouseY);
+    const force = P5.Vector.sub(mousePos, this.pos);
+    return force;
+  };
+
+  connect(link: Link) {
+    if (!link.adjacentNeighbors.includes(this))
+      link.adjacentNeighbors.push(this);
+    if (!this.adjacentNeighbors.includes(link))
+      this.adjacentNeighbors.push(link);
+  }
+
+  connectDiagonal(link: Link) {
+    this.connect(link);
+    if (!link.diagonalNeighbors.includes(this))
+      link.diagonalNeighbors.push(this);
+    if (!this.diagonalNeighbors.includes(link))
+      this.diagonalNeighbors.push(link);
+  }
+
+  calculateSpringForce = (neighbor: Link) => {
+    const force = P5.Vector.sub(neighbor.pos, this.pos);
+    const distance = force.mag();
+    const forceMag = (distance - attractionDist) * this.k;
+    force.setMag(forceMag);
+    return force;
+  };
 }
