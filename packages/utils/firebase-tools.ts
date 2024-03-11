4dbe60cbb332ce0278ec8dc83c934dd68c24564d
Dreadhalor <Scotthetrick2@yahoo.com>
2024-03-10 20:24:52 -0700

    Added fallcrate Fixed firebase imports

--- packages/utils/firebase-tools.ts
+++ packages/utils/firebase-tools.ts
@@ -1,12 +1,24 @@
 import { getApps, initializeApp, getApp } from 'firebase/app';
 import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 import { getAuth, connectAuthEmulator } from 'firebase/auth';
-import { firebaseConfig } from '@repo/config/firebase-config';
+import { connectStorageEmulator, getStorage } from 'firebase/storage';
+
+// Firebase v9+: pull from .env
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 const db = getFirestore(app);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
 // Connect to Firebase auth emulator if the host is localhost
 if (location.hostname === 'localhost') {
@@ -20,4 +32,10 @@ if (location.hostname === 'localhost') {
   connectFirestoreEmulator(db, 'localhost', 8080);
 }
 
-export { auth, db };
+// Connect to Firebase storage emulator if the host is localhost
+if (location.hostname === 'localhost') {
+  // Point to the emulators running on localhost.
+  connectStorageEmulator(storage, 'localhost', 9199);
+}
+
+export { auth, db, storage };
