d2051e1e2bf0dea08a5a77bdeb84e9030af49068
Dreadhalor <Scotthetrick2@yahoo.com>
2024-03-25 04:25:13 -0700

    More configs Setup backend server with admin panel Setup mongodb cloud db Payload admin panel works

---
+++ apps/digitalhippo/src/get-payload.ts
@@ -0,0 +1,55 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import type { InitOptions } from 'payload/config';
+import payload, { Payload } from 'payload';
+import { GlobalConfig } from 'payload/types';
+
+type PayloadCache = {
+  client: Payload | null;
+  promise: Promise<Payload> | null;
+};
+
+dotenv.config({
+  path: path.resolve(__dirname, '../.env'),
+});
+
+let cached = (global as any).payload as PayloadCache | undefined;
+
+if (!cached) {
+  cached = (global as any).payload = {
+    client: null,
+    promise: null,
+  };
+}
+
+type Args = {
+  initOptions?: Partial<InitOptions>;
+};
+export const getPayloadClient = async ({ initOptions }: Args = {}) => {
+  if (!process.env.PAYLOAD_SECRET) {
+    throw new Error('Missing PAYLOAD_SECRET environment variable');
+  }
+
+  if (cached?.client) {
+    return cached.client;
+  }
+
+  if (cached && !cached?.promise) {
+    cached.promise = payload.init({
+      secret: process.env.PAYLOAD_SECRET,
+      local: initOptions?.express ? false : true,
+      ...(initOptions || {}),
+    });
+  }
+
+  try {
+    if (cached) cached.client = (await cached?.promise) ?? null;
+  } catch (error) {
+    if (cached) cached.promise = null;
+    console.error(error);
+    throw error;
+  }
+
+  if (!cached) return null;
+  return cached.client;
+};
