2be973e1f9f84e59480210eb3bb626e8454e6802
Dreadhalor <Scotthetrick2@yahoo.com>
2024-04-17 02:19:18 -0700

    Production webhook test

--- apps/home-page/src/components/page/page-content.tsx
+++ apps/home-page/src/components/page/page-content.tsx
@@ -16,7 +16,7 @@ const PageContent = () => {
       )}
     >
       <Section name='about' className='mb-4'>
-        <SectionHeader>About Mee</SectionHeader>
+        <SectionHeader>About Meqqq</SectionHeader>
         <SectionContent className='flex flex-col gap-4 px-0'>
           <p>
             My web development journey started in high school, automating Model
