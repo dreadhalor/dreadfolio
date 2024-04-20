c29d1ddb4e49415a21a5532c9fba5bc43936c34a
Dreadhalor <Scotthetrick2@yahoo.com>
2024-04-20 04:41:39 -0700

    Up to 20 fish now

--- apps/dredge/src/assets/fish/index.ts
+++ apps/dredge/src/assets/fish/index.ts
@@ -8,3 +8,19 @@ export { default as StingrayImage } from './stingray.webp';
 export { default as SailfishImage } from './sailfish.webp';
 export { default as BronzeWhalerImage } from './bronze-whaler.webp';
 export { default as BlacktipReefSharkImage } from './blacktip-reef-shark.webp';
+export { default as CommonCrabImage } from './common-crab.webp';
+export { default as FiddlerCrabImage } from './fiddler-crab.webp';
+export { default as OceanicPerchImage } from './oceanic-perch.webp';
+export { default as TigerMackerelImage } from './tiger-mackerel.webp';
+// black sea bass
+export { default as BlackSeaBassImage } from './black-sea-bass.webp';
+// stonefish
+export { default as StonefishImage } from './stonefish.webp';
+// blackmouth salmon
+export { default as BlackmouthSalmonImage } from './blackmouth-salmon.webp';
+// conger eel
+export { default as CongerEelImage } from './conger-eel.webp';
+// devil ray
+export { default as DevilRayImage } from './devil-ray.webp';
+// sturgeon
+export { default as SturgeonImage } from './sturgeon.webp';
