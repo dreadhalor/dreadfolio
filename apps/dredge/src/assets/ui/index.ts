1634845f935f6259dc5ba618724ce496dce3c6a3
Dreadhalor <Scotthetrick2@yahoo.com>
2024-04-25 02:44:30 -0700

    Broke out dredge into its own repo

--- apps/dredge/src/assets/ui/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as CargoBackgroundImage } from './dredge-cargo-background.png';
-export { default as DamageImage } from './damage.png';
-export { default as EncyclopediaUnderlineImage } from './encyclopedia-underline.png';
-export { default as TabImage } from './tab.png';
-export { default as BorderImage } from './border.png';
-export { default as AutoPackIcon } from './auto-pack-icon.svg?react';
--- apps/dredge/src/lib/bin-packing/bin-packing.worker.ts
+++
@@ -1,7 +0,0 @@
-import { binPacking } from './bin-packing';
-
-self.onmessage = (event) => {
-  const { items, grid } = event.data;
-  const result = binPacking(items, grid);
-  self.postMessage(result);
-};
