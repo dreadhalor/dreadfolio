45450870e3be964bfd305e7d9c55db6616593f4b
Dreadhalor <Scotthetrick2@yahoo.com>
2024-04-26 22:01:39 -0700

    Starting to make deploy script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Build the Docker image
+pnpm build
+docker-compose build
+
+# Save the Docker image to a tar file
+docker save dreadfolio-portfolio:latest > portfolio.tar
+
+# Transfer the tar file to the EC2 instance over SSH
+scp -i portfolio-key-pair.pem portfolio.tar ec2-user@ec2-54-164-173-195.compute-1.amazonaws.com:~/
+
+# SSH into the EC2 instance
+ssh -i portfolio-key-pair.pem ec2-user@ec2-54-164-173-195.compute-1.amazonaws.com <<'EOF'
+  # Stop the currently running container
+  docker stop $(docker ps -q)
+
+  # Load the transferred Docker image from the tar file
+  docker load < portfolio.tar
+
+  # Run the new container
+  docker run -d -p 80:3000 dreadfolio-portfolio:latest
+
+  # Clean up the tar file
+  rm portfolio.tar
+EOF
